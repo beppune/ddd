f2965cf84298065245c72112e53f193f35a3310f
Giuseppe Manzo <manzo.giuseppe@gmail.com>
2023-11-23 21:30:42 +0100

    either full implementation - not tested

--- src/main/kotlin/org/example/portsexample/model/Either.kt
+++ src/main/kotlin/org/example/portsexample/model/Either.kt
@@ -1,4 +1,5 @@
-package org.example.portsexample
+package org.example.portsexample.model
+
 
 sealed class Either<out L, out R> {
 
@@ -17,6 +18,16 @@ sealed class Either<out L, out R> {
         is Left -> EmptyProjection()
         is Right -> ValueProjection(this.value)
     }
+
+    fun isLeft() = when(this) {
+        is Left -> true
+        is Right -> false
+    }
+
+    fun isRight() = when(this) {
+        is Left -> false
+        is Right -> true
+    }
 }
 
 data class Left<L>(val value: L) : Either<L,Nothing>()
@@ -24,21 +35,25 @@ data class Right<R>(val value: R) : Either<Nothing, R>()
 
 sealed class Projection<out T> {
     abstract fun <U> map(fn: (T) -> U): Projection<U>
+    abstract fun exists(fn: (T)->Boolean = {false}) : Boolean
+    abstract fun orNull() : T?
 }
 
 class ValueProjection<out T>(val value: T) : Projection<T>() {
     override fun <U> map(fn: (T) -> U): Projection<U> =
         ValueProjection(fn(value))
+
+    override fun exists(fn: (T) -> Boolean): Boolean = fn(this.value)
+    override fun orNull(): T? = this.value
 }
 
 class EmptyProjection<out T> : Projection<T>() {
     override fun <U> map(fn: (T) -> U): Projection<U> = EmptyProjection()
+    override fun exists(fn: (T) -> Boolean): Boolean = false
+    override fun orNull(): T? = null
 }
 
 fun <T> Projection<T>.getOrElse(or: () -> T): T = when (this) {
     is EmptyProjection -> or()
     is ValueProjection -> this.value
-}
-
-class TestEither {
 }
